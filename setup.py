5e5f7244d3d705371f6cd0982c1e91a0f1ca26b0
rick duijnisveld <80698367+rickduijn@users.noreply.github.com>
2021-04-07 11:23:41 +0200

    ook hier een jan

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
-
+#ookhierjna
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
